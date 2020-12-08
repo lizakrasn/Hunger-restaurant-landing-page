8c90ecc7c95dfff00998be74dd291a44d86bc6bc
Liza Krasnoshchok <nlight1122@gmail.com>
2020-12-08 19:00:40 +0200

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Hunger-restaurant-landing-page
+
+## Description
+* Stack: HTML5, SASS, CSS animations, GSAP, JS(slider, burgerMenu) Flexbox, Grid, BEM methodology, Webpack.
+  - Mobile (> 320px)
+  - Surface Duo 540px
+  - Tablet 768px
+  - Tablet 1024px
+  - Desktop 1280px
+  - Desktop 1440px
+
+## Local Deployment
+### Installing
+* Run `npm install` in your terminal
+* Run `npm start`to run local dev server on http://localhost:9000
+* Run `npm run build` to publish site to ./dist folder
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import clock from "./scripts/clock";
 import animateText from "./scripts/animateText";
 import addBurgerAutoClose from "./scripts/closeBurgerMenu";
 import submitForm from "./scripts/submitForm";
-import gallery from "./scripts/gallery";
+import slider from "./scripts/gallery";
 
 generateDishItem();
 clock();
 addBurgerAutoClose();
 submitForm();
-gallery();
+sloder();
 
 const navigationItems = document.querySelectorAll('.navigation__item-link')
 navigationItems.forEach(navigationItem => animateText(navigationItem));
